87c1beeca0c54656baff2ae9fe44a124af8367e5
Paddle <PaddleStroke@users.noreply.github.com>
2022-03-25 17:25:41 +0100

    Sketcher : Implement chamfer in fillet tool. - Adapt Fillet to new DSH with widget support. - Add chamfer and complex corners support with widget. - remove fillet conserving point as it's integrated in normal fillet. - Remove fillet comp as all is included in fillet tool.

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -892,7 +892,6 @@ bool ViewProviderSketch::mouseButtonPressed(int Button, bool pressed, const SbVe
                                   << "Sketcher_CreateRectangle"
                                   << "Sketcher_CreateHexagon"
                                   << "Sketcher_CreateFillet"
-                                  << "Sketcher_CreatePointFillet"
                                   << "Sketcher_Trimming"
                                   << "Sketcher_Extend"
                                   << "Sketcher_External"

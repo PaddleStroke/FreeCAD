32a1086d43608d1c6b99d73f24bca6c985c3634d
Paddle <PaddleStroke@users.noreply.github.com>
2022-07-07 10:37:58 +0200

    DSH Slot : add getstate

--- src/Mod/Sketcher/Gui/DrawSketchHandlerSlot.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerSlot.h
@@ -246,7 +246,23 @@ private:
     }
 };
 
-
+template <> auto DrawSketchHandlerSlotBase::ToolWidgetManager::getState(int parameterindex) const {
+    switch (parameterindex) {
+    case WParameter::First:
+    case WParameter::Second:
+        return SelectMode::SeekFirst;
+        break;
+    case WParameter::Third:
+    case WParameter::Fourth:
+        return SelectMode::SeekSecond;
+        break;
+    case WParameter::Fifth:
+        return SelectMode::SeekThird;
+        break;
+    default:
+        THROWM(Base::ValueError, "Parameter index without an associated machine state")
+    }
+}
 
 template <> void DrawSketchHandlerSlotBase::ToolWidgetManager::configureToolWidget() {
     toolWidget->setParameterLabel(WParameter::First, QApplication::translate("ToolWidgetManager_p1", "x of 1st point"));

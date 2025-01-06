51b94908e5c85c6700fc6d6162088a445b06500f
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-01-06 18:25:27 +0100

    Core: add Tool Group command. And Addon manager proxy.

--- src/Gui/Application.cpp
+++ src/Gui/Application.cpp
@@ -1038,6 +1038,7 @@ void Application::createStandardOperations()
     Gui::CreateStructureCommands();
     Gui::CreateTestCommands();
     Gui::CreateLinkCommands();
+    Gui::CreateStdGroupCommands();
 }
 
 void Application::slotNewDocument(const App::Document& Doc, bool isMainDoc)

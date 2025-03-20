01261ad0000bfb80679a9ea5811dd5f50dad8457
PaddleStroke <pierrelouis.boyer@gmail.com>
2025-03-20 07:28:09 +0100

    Sketcher: Change distanceX/Y shortcut so that polyline can use L

--- src/Mod/Sketcher/Gui/CommandConstraints.cpp
+++ src/Mod/Sketcher/Gui/CommandConstraints.cpp
@@ -5244,7 +5244,7 @@ CmdSketcherConstrainDistanceX::CmdSketcherConstrainDistanceX()
     sWhatsThis = "Sketcher_ConstrainDistanceX";
     sStatusTip = sToolTipText;
     sPixmap = "Constraint_HorizontalDistance";
-    sAccel = "L";
+    sAccel = "K, L";
     eType = ForEdit;
 
     // Can't do single vertex because its a prefix for 2 vertices
@@ -5545,7 +5545,7 @@ CmdSketcherConstrainDistanceY::CmdSketcherConstrainDistanceY()
     sWhatsThis = "Sketcher_ConstrainDistanceY";
     sStatusTip = sToolTipText;
     sPixmap = "Constraint_VerticalDistance";
-    sAccel = "I";
+    sAccel = "K, I";
     eType = ForEdit;
 
     // Can't do single vertex because its a prefix for 2 vertices

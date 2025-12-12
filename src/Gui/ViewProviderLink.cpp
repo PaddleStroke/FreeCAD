415cd24abad7b88850a1a2564a62bc022146e9b2
paddle <pierrelouis.boyer@gmail.com>
2025-12-12 10:07:39 +0100

    Link: add mirror action. - PR

--- src/Gui/ViewProviderLink.cpp
+++ src/Gui/ViewProviderLink.cpp
@@ -3204,6 +3204,26 @@ void ViewProviderLink::_setupContextMenu(
         );
     }
 
+    if (ext->getScaleProperty() && !ext->getScaleProperty()->isReadOnly()) {
+        Gui::Document* doc = getDocument();
+        auto prop = ext->getScaleProperty();
+        auto action = menu->addAction(QObject::tr("Mirror link"), [prop, doc] {
+            try {
+                App::AutoTransaction guard(
+                    doc->openCommand(QT_TRANSLATE_NOOP("Command", "Mirror link"))
+                );
+                prop->setValue(prop->getValue() * -1.0);
+                Command::updateActive();
+            }
+            catch (Base::Exception& e) {
+                e.reportException();
+            }
+        });
+        action->setCheckable(true);
+        action->setChecked(prop->getValue() < 0.0);
+        action->setToolTip(QObject::tr("This affect only the selected link. The original object remains unchanged."));
+    }
+
     if (ext->getColoredElementsProperty()) {
         bool found = false;
         const auto actions = menu->actions();

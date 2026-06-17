cfae984771c2063e19c98b44da295e38a3e8f90a
paddle <pierrelouis.boyer@gmail.com>
2026-06-17 15:09:48 +0200

    Part: Link arrays

--- src/Mod/PartDesign/Gui/Workbench.cpp
+++ src/Mod/PartDesign/Gui/Workbench.cpp
@@ -350,6 +350,9 @@ void Workbench::activated()
     const char* Transformed[] = {
         "PartDesign_Mirrored",
         "PartDesign_LinearPattern",
+        "PartDesign_CircularPattern",
+        "PartDesign_PathPattern",
+        "PartDesign_PointPattern",
         "PartDesign_PolarPattern",
         "PartDesign_MultiTransform",
         nullptr
@@ -431,6 +434,9 @@ Gui::MenuItem* Workbench::setupMenuBar() const
     *transformations << "PartDesign_Mirrored"
                      << "PartDesign_LinearPattern"
                      << "PartDesign_PolarPattern"
+                     << "PartDesign_CircularPattern"
+                     << "PartDesign_PathPattern"
+                     << "PartDesign_PointPattern"
                      << "PartDesign_MultiTransform";
 
     // dressups
@@ -528,6 +534,9 @@ Gui::ToolBarItem* Workbench::setupToolBars() const
 
     *part << "PartDesign_Mirrored"
           << "PartDesign_LinearPattern"
+          << "PartDesign_CircularPattern"
+          << "PartDesign_PathPattern"
+          << "PartDesign_PointPattern"
           << "PartDesign_PolarPattern"
           << "PartDesign_MultiTransform";
 

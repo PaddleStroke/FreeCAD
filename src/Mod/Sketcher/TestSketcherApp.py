d5d39e21293f2ef4bd6b51b1b340e8c4296b54c7
paddle <pierrelouis.boyer@gmail.com>
2026-09-08 13:51:37 +0200

    Sketcher: Recursive groups implementation 28276

--- src/Mod/Sketcher/TestSketcherApp.py
+++ src/Mod/Sketcher/TestSketcherApp.py
@@ -30,6 +30,7 @@ from SketcherTests.TestSketchExpression import TestSketchExpression
 from SketcherTests.TestSketchValidateCoincidents import TestSketchValidateCoincidents
 from SketcherTests.TestSketchCarbonCopyReverseMapping import TestSketchCarbonCopyReverseMapping
 from SketcherTests.TestSketchInternalFaces import TestSketchInternalFaces
+from SketcherTests.TestSketchGroups import TestSketchGroups
 
 # Path and PartDesign tests use these functions that used to live here
 # but moved to SketcherTests/TestSketcherSolver.py

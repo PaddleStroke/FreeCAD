d63e65a11b21192461a44ed6afb51765272c2555
paddle <pierrelouis.boyer@gmail.com>
2026-09-09 21:07:32 +0200

    Part: add polar link arrays and placement tests

--- src/Mod/Part/App/AppPart.cpp
+++ src/Mod/Part/App/AppPart.cpp
@@ -100,6 +100,7 @@
 #include "LinkArray.h"
 #include "LinkArrayCircular.h"
 #include "LinkArrayLinear.h"
+#include "LinkArrayPolar.h"
 #include "PolarPatternExtension.h"
 #include "PathPatternExtension.h"
 #include "Geometry.h"
@@ -463,6 +464,7 @@ PyMOD_INIT_FUNC(Part)
     Part::LinkArray             ::init();
     Part::LinkArrayCircular     ::init();
     Part::LinkArrayLinear       ::init();
+    Part::LinkArrayPolar        ::init();
 
     Part::Feature               ::init();
     Part::FeatureExt            ::init();
